4dae06fcdb30084cd6fe6454b8c44cb8b2012edb
Mirlind Murati <murati@ut.ee>
2020-10-08 17:43:22 +0300

    VendorList and VendorRegister added. Reset button works.

---
+++ src/views/vendors/VendorRegister.js
@@ -0,0 +1,122 @@
+import React, {Component} from 'react';
+import {
+    CButton,
+    CCard,
+    CCardBody,
+    CCardFooter,
+    CCardHeader,
+    CCol,
+    CForm,
+    CFormGroup,
+    CTextarea,
+    CInput,
+    CLabel,
+    CSelect,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import * as All from '@fortawesome/free-solid-svg-icons'
+
+class VendorRegister extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          inputName: '',
+          inputAddress: '',
+          inputEmail: ''
+        };
+      }
+
+    inputChangeHandler = (event) => {
+        event.preventDefault();
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    resetInputHandler = () => {
+        this.setState({
+            inputName: '',
+            inputAddress: '',
+            inputEmail: ''
+        })
+    }
+
+    render() {
+        return (
+            <CCard>
+                <CCardHeader>Register New Vendor</CCardHeader>
+                <CCardBody>
+                    <CForm action="" method="post" onReset={this.resetInputHandler} encType="multipart/form-data" className="form-horizontal">
+                    <CFormGroup row>
+                            <CCol md="3">
+                                <CLabel htmlFor="email-input">Vendor Name</CLabel>
+                            </CCol>
+                            <CCol xs="12" md="9">
+                                <CInput type="text" 
+                                value={this.state.inputName} 
+                                onChange={this.inputChangeHandler}
+                                id="name-input" 
+                                name="inputName" 
+                                placeholder="Enter Name" 
+                                autoComplete="name" /> 
+                                
+                            </CCol> 
+                            
+                        </CFormGroup>
+    
+                        <CFormGroup row>
+                            <CCol md="3">
+                                <CLabel htmlFor="email-input">Email Address</CLabel>
+                            </CCol>
+                            <CCol xs="12" md="9">
+                                <CInput 
+                                type="email" 
+                                value={this.state.inputEmail} 
+                                onChange={this.inputChangeHandler}
+                                id="email-input" 
+                                name="inputEmail" 
+                                placeholder="Enter Email" 
+                                autoComplete="email" />
+                            </CCol>
+                        </CFormGroup>
+    
+                        <CFormGroup row>
+                            <CCol md="3">
+                                <CLabel htmlFor="address-input">Home Address</CLabel>
+                            </CCol>
+                            <CCol xs="12" md="9">
+                                <CInput
+                                    name="inputAddress"
+                                    value={this.state.inputAddress} 
+                                    onChange={this.inputChangeHandler}
+                                    id="address-input"
+                                    rows="2"
+                                    placeholder="Home Address..."
+                                />
+                            </CCol>
+                        </CFormGroup>
+
+                <CCardFooter >
+                    <CButton style={{marginRight:"30px", marginLeft: "-20px"}} type="submit" size="md" color="success"><FontAwesomeIcon icon={All.faCheckCircle} />Submit</CButton>
+                    <CButton style={{marginRight:"30px"}} type="reset" size="md" color="danger"> <FontAwesomeIcon icon={All.faCircle} /> Reset</CButton> 
+                    <CButton style={{marginRight:"30px"}} to={"/vendor/vendorList"} type="reset" size="md" color="primary"> <FontAwesomeIcon  icon={All.faArrowAltCircleLeft} /> Back</CButton>
+                </CCardFooter>
+    
+                    </CForm>
+                </CCardBody>
+                {/* <CCardFooter >
+                    <CButton style={{marginRight:"30px"}} type="submit" size="md" color="success"><FontAwesomeIcon icon={All.faCheckCircle} />Submit</CButton>
+                    <CButton style={{marginRight:"30px"}} type="reset" size="md" color="danger"> <FontAwesomeIcon icon={All.faCircle} /> Reset</CButton> 
+                    <CButton style={{marginRight:"30px"}} to={"/vendor/vendorList"} type="reset" size="md" color="primary"> <FontAwesomeIcon  icon={All.faArrowAltCircleLeft} /> Back</CButton>
+                </CCardFooter> */}
+            </CCard>
+        )
+
+    }
+    
+}
+
+export default VendorRegister
