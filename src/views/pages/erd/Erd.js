ca8b04bd74f425d45234afba75a2050b5d70ed6e
abdul0214 <abdul.wahab0214@gmail.com>
2020-10-29 17:17:04 +0200

    Added Erd Diagram at '/#/erd' endpoint

---
+++ src/views/pages/erd/Erd.js
@@ -0,0 +1,29 @@
+import React from 'react'
+// import {
+//   CButton,
+//   CCard,
+//   CCardBody,
+//   CCardGroup,
+//   CCol,
+//   CContainer,
+//   CForm,
+//   CInput,
+//   CInputGroup,
+//   CInputGroupPrepend,
+//   CInputGroupText,
+//   CRow
+// } from '@coreui/react'
+// import CIcon from '@coreui/icons-react'
+import ErdDiagram from '../../../assets/HomeAssetClassDiagram.png'
+// import './Login.css'
+
+const Login = () => {
+
+  return (
+    <div className="c-app c-default-layout flex-row align-items-center">
+                    <img src={ErdDiagram} style={{ width: '70%'}} className='img-rotate' alt="Siktir"/>
+    </div>
+  )
+}
+
+export default Login
