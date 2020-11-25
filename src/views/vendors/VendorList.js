ef4b9256ad271b003ce1622dd633a897c4cd8b3d
Mirlind Murati <mirlindmurati777@gmail.com>
2020-11-25 20:53:45 +0200

    AW-43 #comment CRUD Vendor Implemented #done

--- src/views/vendors/VendorList.js
+++ src/views/vendors/VendorList.js
@@ -7,6 +7,12 @@ import {
     CDataTable,
     CLabel,
     CInput,
+    CForm,
+    CFormGroup,
+    CCol,
+    CTextarea,
+    CCardFooter,
+    CButton,
 } from '@coreui/react';
 import { Link } from 'react-router-dom';
 //import vendorsData from '../resources/VendorsData';
@@ -36,6 +42,14 @@ class VendorList extends Component {
             vendors: [],
             vendorByID: {},
             vendorID: null,
+            deletedVendorByID: {},
+
+            //for put request,
+            updateVendorID: '',
+            updateVendorName: '',
+            updateVendorHomeAddress: '',
+            updateVendorEmailAddress: '',
+            updatedVendor: {},
         }
     }
 
@@ -52,8 +66,7 @@ class VendorList extends Component {
                     headers:
                     {
                         'Authorization': 'Bearer ' + this.state.token,
-                        "Content-Type": "application/json",
-                        //"Access-Control-Allow-Headers": "X-Requested-With, content-type",
+                        "Content-Type": "application/json",                        
                     }}).then(response => {
                         this.setState({vendors: response.data._embedded.vendorDTOList })   
                         console.log(response)
@@ -100,6 +113,77 @@ class VendorList extends Component {
             }).catch(error => console.log(error.toString()));
     }
 
+    inputChangeHandler = (event) => {
+        event.preventDefault();
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    }
+
+    // Update Vendor Info Request
+    updateVendorInfoHandler = (event) => {
+        event.preventDefault();
+
+        console.log("Here 1");
+        let vendor = {
+            id: this.state.updateVendorID,
+            name: this.state.updateVendorName,
+            address: this.state.updateVendorHomeAddress,
+            email: this.state.updateVendorEmailAddress,
+        }
+        console.log("Here 2");
+        axios.post('http://localhost:8070/api/authenticate',
+        {
+            "username":"admin",
+            "password":"admin",
+        }).then(response =>{
+            this.setState({token: response.data.accessToken});
+            console.log(this.state.token)  
+            //console.log(vendorID)                 
+            axios.put('http://localhost:8070/api/hr/vendors/', vendor,
+            {
+                headers:
+                {
+                    'Authorization': 'Bearer ' + this.state.token,
+                    "Content-Type": "application/json",
+                    //"Access-Control-Allow-Headers": "X-Requested-With, content-type",
+                }}).then(response => {
+                    this.setState({
+                        updatedVendor: response.data
+                    })   
+                    console.log(response)
+                })
+                .catch(error => console.log(error.toString()))                   
+        }).catch(error => console.log(error.toString()));
+    }
+
+
+
+    deleteVendorByID = (event) => {
+        let vendorID = this.state.vendorID;
+
+        axios.post('http://localhost:8070/api/authenticate',
+            {
+                "username":"admin",
+                "password":"admin",
+            }).then(response =>{
+                this.setState({token: response.data.accessToken});
+                console.log(this.state.token)  
+                console.log(vendorID)                 
+                axios.delete(`http://localhost:8070/api/hr/vendors/${vendorID}`,
+                {
+                    headers:
+                    {
+                        'Authorization': 'Bearer ' + this.state.token,
+                        "Content-Type": "application/json",
+                        //"Access-Control-Allow-Headers": "X-Requested-With, content-type",
+                    }}).then(response => {
+                        this.setState({deletedVendorByID: response.data})   
+                        console.log(response)
+                    })
+                    .catch(error => console.log(error.toString()))                   
+            }).catch(error => console.log(error.toString()));
+    }
 
     render () {
         return (
@@ -136,10 +220,11 @@ class VendorList extends Component {
                 </CCardBody>
             </CCard>
 
+            {/* Fetch Vendor by its ID */}
             <CCard>
                 <CCardHeader style={{fontSize: "15px"}}>                    
                     <div>
-                    <CLabel style={{}}>Search for a Vendor by the Id</CLabel> <br/>
+                    <CLabel style={{}}>Search for a Vendor</CLabel> <br/>
                     <CInput
                         type="text"
                         id="vendor-id"
@@ -170,33 +255,144 @@ class VendorList extends Component {
                             </tr>                          
                         </tbody>
                         </table>
-                    {/* <CDataTable style={{color: "blue"}}
-                        items={this.state.vendorByID}
-                        fields={fields}
-                        light
-                        hover
-                        striped
-                        outlined
-                        size="m"
-                        itemsPerPage={5}
-                        pagination
-                        scopedSlots={{
-                            'status':
-                                (item) => (
-                                    <td>
-                                        <CBadge color={getBadge(item.status)}>
-                                            {item.status}
-                                        </CBadge>
-                                    </td>
-                                )
-                        }}
-                    /> */}
                 </CCardBody>
             </CCard>
 
+            {/* Update Vendor Info */}
+            <CCard>
+                <CCardHeader>Update Vendor's Information</CCardHeader>
+                <CCardBody>
+                    <CForm id="update-vendor" onSubmit={this.updateVendorInfoHandler} encType="multipart/form-data" className="form-horizontal">
+                        <CFormGroup row>
+                            <CCol md="3">
+                                <CLabel htmlFor="email-input">ID</CLabel>
+                            </CCol>
+                            <CCol xs="12" md="9">
+                                <CInput type="text" 
+                                value={this.state.inputName} 
+                                onChange={this.inputChangeHandler}
+                                id="vendorID" 
+                                name="updateVendorID" 
+                                placeholder="Enter New ID" 
+                                autoComplete="id" />                                 
+                            </CCol>                             
+                        </CFormGroup>
+
+                        <CFormGroup row>
+                            <CCol md="3">
+                                <CLabel htmlFor="email-input">Name</CLabel>
+                            </CCol>
+                            <CCol xs="12" md="9">
+                                <CInput type="text" 
+                                value={this.state.inputName} 
+                                onChange={this.inputChangeHandler}
+                                id="vendorName" 
+                                name="updateVendorName" 
+                                placeholder="Enter New Name" 
+                                autoComplete="name" />                                 
+                            </CCol>                             
+                        </CFormGroup>
+
+                        <CFormGroup row>
+                            <CCol md="3">
+                                <CLabel htmlFor="address-input">Home Address</CLabel>
+                            </CCol>
+                            <CCol xs="12" md="9">
+                                <CTextarea
+                                    name="updateVendorHomeAddress"
+                                    id="homeAddress"
+                                    // value={this.state.inputAddress} 
+                                    onChange={this.inputChangeHandler}
+                                    rows="2"
+                                    placeholder="Enter New Home Address"
+                                />                            
+                            </CCol>
+                        </CFormGroup>
+    
+                        <CFormGroup row>
+                            <CCol md="3">
+                                <CLabel htmlFor="email-input">Email Address</CLabel>
+                            </CCol>
+                            <CCol xs="12" md="9">
+                                <CInput 
+                                type="email" 
+                                value={this.state.inputEmail} 
+                                onChange={this.inputChangeHandler}
+                                id="emailAddress" 
+                                name="updateVendorEmailAddress" 
+                                placeholder="Enter New Email Address" 
+                                autoComplete="email" />
+                            </CCol>
+                        </CFormGroup>                        
+                        <CCardFooter >
+                            <CButton style={{marginRight:"30px", marginLeft: "-20px"}} type="submit" size="md" color="success"><FontAwesomeIcon icon={All.faCheckCircle} />Update</CButton>
+                        </CCardFooter>                                                
+                    </CForm>                   
+                </CCardBody>
+                <CCardHeader>The Updated Vendor is</CCardHeader>
+                    <CCardBody>
+                                <table className="table table-hover">
+                                    <thead>
+                                        <tr>
+                                        <th scope="col">Id</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Address</th>
+                                        <th scope="col">Email</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                        <th scope="row">{this.state.updatedVendor.id}</th>
+                                        <td>{this.state.updatedVendor.name}</td>
+                                        <td>{this.state.updatedVendor.address}</td>
+                                        <td>{this.state.updatedVendor.email}</td>
+                                        </tr>                          
+                                    </tbody>
+                                    </table> 
+                                </CCardBody>  
+            </CCard>
+
+          
+            {/* Delete a Vendor by ID */}
+            <CCard>
+                <CCardHeader style={{fontSize: "15px"}}>                    
+                    <div>
+                    <CLabel style={{}}>Delete a Vendor</CLabel> <br/>
+                    <CInput
+                        type="text"
+                        id="vendor-id"
+                        name="vendorID" onChange={this.onVendorIDChangeHandler}
+                        placeholder="Enter Vendor ID.."
+                        autoComplete="off"
+                        style={{width: "400px", float:"left", marginRight: "10px"}}
+                    />  <button onClick={this.deleteVendorByID} type="button" style={{float: "left"}} className="btn btn-info">Delete Vendor</button>
+                    {/* <FontAwesomeIcon icon={All.faUserPlus} /> <Link to={"/vendor/vendorRegister"} className="card-header-action">Register New Vendor</Link> */}
+                    </div>
+                </CCardHeader>
+                <CCardBody>
+                <CLabel style={{}}>The Deleted Vendor is: </CLabel> <br/>
+                    <table className="table table-hover">
+                        <thead>
+                            <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Address</th>
+                            <th scope="col">Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <th scope="row">{this.state.deletedVendorByID.id}</th>
+                            <td>{this.state.deletedVendorByID.name}</td>
+                            <td>{this.state.deletedVendorByID.address}</td>
+                            <td>{this.state.deletedVendorByID.email}</td>
+                            </tr>                          
+                        </tbody>
+                        </table>
+                </CCardBody>
+            </CCard>
             </>
-                )
-    }
+                )};
 }
 
 export default VendorList
